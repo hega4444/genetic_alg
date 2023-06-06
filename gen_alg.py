10435813c18904e4c6c0d4497d5fd4dec9b7800f
hermip <hernan.mip@gmail.com>
2023-06-06 14:11:30 +0200

    changed README and other minor changes in names

--- gen_alg.py
+++ gen_alg.py
@@ -1,12 +1,21 @@
 import string
 import random
+import math
 
-#PARAMETERS
-STRING_LENGTH = 4
-N_GENERATIONS = 2
-N_SOL_PER_GEN = 100     #number of solutions generated in a generation
-MUTATION_RATE = 0.01    #need to check here
-ELITISM = 0.1           #% of selections that move to next generation unaltered
+# DEFAULT PARAMETERS
+STRING_LENGTH = 4           #length of target string
+N_GENERATIONS = 2           #number of generations
+N_SOL_PER_GEN = 100         #number of solutions generated in a generation
+MUTATION_RATE = 0.01        #need to check here
+ELITSM = 0.1                #% of selections that move to next generation unaltered
+
+
+#assign internal variables (allow user adjust parameters)
+string_length = STRING_LENGTH
+n_generations = N_GENERATIONS
+n_sol_per_gen = N_SOL_PER_GEN
+mutation_rate = MUTATION_RATE    
+elitism = ELITSM           
 
 
 def gen_random_string(length): #function to create random solutions
@@ -24,28 +33,37 @@ def fitness(goal, pos_solution): #function to evaluate fitness of a given soluti
 
 #begin of main program
 
-target = input(f'Enter a target string (Max. nr. of characters {STRING_LENGTH} and lowercase):')
+target = input(f'Enter a target string (Max. nr. of characters {string_length} and lowercase):')
 #adjust target in case of more / less cgaracters than expected
-if len(target)>STRING_LENGTH:      #truncate
-    target = target[:STRING_LENGTH]
-elif len(target)<STRING_LENGTH:
-    target = target + ''.join(' ' for i in range(STRING_LENGTH-len(target)))  #fill with spaces
+if len(target)>string_length:      #truncate
+    target = target[:string_length]
+elif len(target)<string_length:
+    target = target + ''.join(' ' for i in range(string_length-len(target)))  #fill with spaces
 
+
+#calculate worst possible fitness value for the number of characters
+worst_fitness = fitness('a','z') * string_length
 
 solutions_gen = [] #list for generated solutions
 
-for i in range(N_SOL_PER_GEN):
-    gen = gen_random_string(STRING_LENGTH)  #create first generation of solutions 
-    fit = fitness (target , gen)            #determine the fitness of random generated solution
+for i in range(n_sol_per_gen):
+    curr_sol = gen_random_string(string_length)     #create first generation of solutions 
+    curr_fit = fitness (target , curr_sol)          #determine the fitness of random generated solution
 
-    gen_tuple = (gen, fit)                  #create a tuple with solutuon and fitness function
+    gen_tuple = (curr_sol, curr_fit)                #create a tuple with solutuon and fitness function
 
-    solutions_gen.append(gen_tuple)         #append solution to list
+    solutions_gen.append(gen_tuple)                 #append solution to list
+
+    mating_chances = math.floor(curr_fit * 100 / worst_fitness)  #calculate mating chances
+
+
+
 
 solutions_gen.sort(key=lambda x: x[1])      #sort solutions by their fitness function
 
 for i in solutions_gen:
     print(i)
+
 
 
 
